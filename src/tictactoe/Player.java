fa29b49e79ddfd906fa58b8326bdddb64d6f5c2d
jozwik <juzwik@gmail.com>
2021-06-25 19:21:27 +0200

    add exception handling in Player class

--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -23,7 +23,12 @@ public class Player extends Behaviour{
     }
 
     int askForProperInput (){
-        int position = Integer.parseInt(input.next());
+        int position = 0;
+        try {
+            position = Integer.parseInt(input.next());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
         while ((position < 0) || (position > 9)){
             System.out.println("Please select proper input (1-9)");
             position = Integer.parseInt(input.next());
