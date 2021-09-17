2e47a1ac05281553b15ca8a753846dfcfc555720
jozwik12 <szymonjozwicki@gmail.com>
2021-09-17 20:16:37 +0200

    make positions map final

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -11,7 +11,7 @@ public class Board {
            									{' ', '|', ' ', '|', ' '}
     };
 
-    private static Map<Integer, Position> positions = Map.of(
+    private static final Map<Integer, Position> positions = Map.of(
             1, new Position(0, 0),
             2, new Position(0, 2),
             3, new Position(0, 4),
